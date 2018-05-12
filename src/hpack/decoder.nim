3a3a557116951602e372e1bfca29b9db629151e3
nitely <ecastroborsani@gmail.com>
2018-05-11 22:00:46 -0300

    implement dyn table evictions

--- src/hpack/decoder.nim
+++ src/hpack/decoder.nim
@@ -5,11 +5,16 @@ import deques
 import huffman_decoder
 import headers_data
 
+type
+  DecodeError = object of ValueError
+
+template raiseDecodeError(msg: string) =
+  raise newException(DecodeError, msg)
+
 proc intdecode*(s: openArray[byte], n: int, d: var int): int =
   ## Return number of consumed octets.
-  ## Return ``-1`` on error.
   ## ``n`` param is the N-bit prefix.
-  ## Decoded int is assigned to ``d``.
+  ## Decoded int is assigned to ``d``
   assert n in {1 .. 8}
   assert len(s) > 0
   d = 1 shl n - 1
@@ -27,18 +32,16 @@ proc intdecode*(s: openArray[byte], n: int, d: var int): int =
     cb = s[result].int
     b = cb and 0x7f
     if b > sf:
-      result = -1
-      return
+      raiseDecodeError("overflow")
     b = b shl m
-    if d > int.high - b:
-      result = -1
-      return
+    if d > int.high-b:
+      raiseDecodeError("overflow")
     inc(d, b)
     inc(m, 7)
     sf = sf shr 7
     inc result
   if cb shr 7 == 1:
-    result = -1
+    raiseDecodeError("continuation byte without continuation")
 
 # todo: add neverIndex flag?
 type
@@ -82,7 +85,7 @@ proc add*(d: var DecodedStr, s: string) =
   d.s.add(s)
   d.b.add(d.s.len)
 
-iterator items(d: DecodedStr): DecodedSlice {.inline.} =
+iterator items*(d: DecodedStr): DecodedSlice {.inline.} =
   ## Iterate over header names and values
   ##
   ## .. code-block:: nim
@@ -107,25 +110,17 @@ iterator items(d: DecodedStr): DecodedSlice {.inline.} =
 proc strdecode(s: openArray[byte], d: var DecodedStr): int =
   ## Decode a literal string.
   ## Return number of consumed octets.
-  ## Return ``-1`` on error.
   ## Decoded string is appended to ``d``.
   assert len(s) > 0
-  var dint = 0
-  let n = intdecode(s, 7, dint)
-  if n == -1:
-    result = -1
-    return
-  if dint > int.high-n:
-    result = -1
-    return
-  result = n+dint
-  if result > len(s):
-    result = -1
-    return
+  let n = intdecode(s, 7, result)
+  if result > int.high-n:
+    raiseDecodeError("overflow")
+  inc(result, n)
+  if result > s.len:
+    raiseDecodeError("out of bounds")
   if s[0] shr 7 == 1:  # huffman encoded
     if hcdecode(toOpenArray(s, n, result-1), d.s) == -1:
-      result = -1
-      return
+      raiseDecodeError("huffman error")
     d.b.add(d.s.len)
   else:
     let j = d.s.len
@@ -138,45 +133,93 @@ type
   Header = object
     h: string
     b: int
-  DynHeaders = Deque[Header]
-    ## Headers dynamic list
 
-proc `$`[T: Header](d: Deque[T]): string {.inline.} =
+proc len(h: Header): int {.inline.} =
+  h.h.len
+
+type
+  DynHeaders = object
+    ## Headers dynamic list
+    d: Deque[Header]
+    cap: int
+    filled: int
+
+proc initDynHeaders*(cap: Natural): DynHeaders {.inline.} =
+  ## Initialize ``DynHeaders``. The ``cap``
+  ## is the capacity of the queue in octets.
+  ## There is no way to set the number of
+  ## headers it can hold.
+  assert cap > 32
+  DynHeaders(
+    d: initDeque[Header](32),
+    cap: cap,
+    filled: 0)
+
+proc `[]`*(dh: DynHeaders, i: int): Header {.inline.} =
+  dh.d[i]
+
+proc len*(dh: DynHeaders): int {.inline.} =
+  dh.d.len
+
+proc pop*(dh: var DynHeaders): Header {.inline.} =
+  result = dh.d.popLast()
+  dec(dh.filled, result.len)
+  assert dh.filled >= 0
+
+proc add*(dh: var DynHeaders, h: Header) {.inline.} =
+  assert dh.filled <= dh.cap
+  while dh.len > 0 and h.len > dh.cap-dh.filled-32:
+    discard dh.pop()
+  if h.len > dh.cap-32:
+    raiseDecodeError("header too long")
+  dh.d.addFirst(h)
+  dh.filled = dh.filled + h.len + 32
+  assert dh.filled <= dh.cap
+
+iterator items*(dh: DynHeaders): Header {.inline.} =
+  for h in dh.d:
+    yield h
+
+proc `$`(dh: DynHeaders): string {.inline.} =
   ## Use it for debugging purposes only
   result = ""
-  for h in d:
+  for h in dh:
     result.add(h.h[0 ..< h.b])
     result.add(": ")
     result.add(h.h[h.b ..< h.h.len])
     result.add("\r\L")
 
-proc hname(h: DynHeaders, d: var DecodedStr, i: int): int =
+proc hname(h: DynHeaders, d: var DecodedStr, i: int) =
   assert i > 0
-  result = 0
-  var i = i-1
+  let
+    i = i-1
+    idyn = i-headersTable.len
   if i < len(headersTable):
-    echo headersTable[i][0]
     d.add(headersTable[i][0])
-  elif i < i-len(headersTable):
-    let hr = h[i-len(headersTable)]
-    echo hr.h[0 ..< hr.b]
-    d.add(hr.h[0 ..< hr.b])
+    # todo: fixme?
+    #d.add("")
+  elif idyn < h.len:
+    # todo: fixme, no extra copy
+    d.add(h[idyn].h[0 ..< h[idyn].b])
+    # todo: fixme?
+    #d.add("")
   else:
-    result = -1
+    raiseDecodeError("dyn header name not found")
 
-proc header(h: DynHeaders, d: var DecodedStr, i: int): int =
+proc header(h: DynHeaders, d: var DecodedStr, i: int) =
   assert i > 0
-  result = 0
-  var i = i-1
-  if i < len(headersTable):
-    echo headersTable[i]
+  let
+    i = i-1
+    idyn = i-headersTable.len
+  if i < headersTable.len:
     d.add(headersTable[i][0])
     d.add(headersTable[i][1])
-  elif i < i-len(headersTable):
-    echo h[i-len(headersTable)]
-    d.add(h[i-len(headersTable)].h)
+  elif idyn < h.len:
+    # todo: fixme, no extra copy
+    d.add(h[idyn].h[0 ..< h[idyn].b])
+    d.add(h[idyn].h[h[idyn].b ..< h[idyn].h.len])
   else:
-    result = -1
+    raiseDecodeError("dyn header not found")
 
 proc litdecode(
     s: openArray[byte],
@@ -188,56 +231,37 @@ proc litdecode(
   ## with incremental indexing,
   ## without indexing, or
   ## never indexed.
-  ## Return number of consmed
-  ## octets, or -1 on error
+  ## Return number of consumed octets
   result = 0
   var dint = 0
-  let n = intdecode(s, np, dint)
-  if n == -1:
-    result = -1
-    return
-  inc(result, n)
+  inc(result, intdecode(s, np, dint))
   if dint > 0:
-    if hname(h, d, dint) == -1:
-      result = -1
-      return
+    hname(h, d, dint)
   else:
     # todo: check 1 < len(s)
     let nh = strdecode(toOpenArray(s, result, s.len-1), d)
-    if nh == -1:
-      result = -1
-      return
     inc(result, nh)
   # todo: check 1+nh < len(s) and does not overflow
   let nv = strdecode(toOpenArray(s, result, s.len-1), d)
-  if nv == -1:
-    result = -1
-    return
   inc(result, nv)
   when store:
     let hsl = d[^1]
-    h.addFirst(Header(
+    h.add(Header(
       # todo: this makes 2 copies, fix
       h: d.s[hsl.n.a .. hsl.v.b],
       b: hsl.n.b - hsl.n.a + 1))
 
 proc hdecode(s: openArray[byte], h: var DynHeaders, d: var DecodedStr): int =
   ## Decode a header.
-  ## Return number of consmed
-  ## octets, or -1 on error
+  ## Return number of consmed octets
   assert len(s) > 0
   # indexed
   if s[0] shr 7 == 1:
     var dint = 0
     result = intdecode(s, 7, dint)
-    if result == -1:
-      return
     if dint == 0:
-      result = -1
-      return
-    if header(h, d, dint) == -1:
-      result = -1
-      return
+      raiseDecodeError("invalid header index 0")
+    header(h, d, dint)
     return
   # incremental indexing
   if s[0] shr 6 == 1:
@@ -249,6 +273,7 @@ proc hdecode(s: openArray[byte], h: var DynHeaders, d: var DecodedStr): int =
   if s[0] shr 4 <= 1:
     result = litdecode(s, h, d, 4, false)
     return
+  raiseDecodeError("unknown octet prefix")
 
 when isMainModule:
   block:
@@ -279,7 +304,6 @@ when isMainModule:
     ds.add(v2)
     doAssert ds.s[ds[^1].n] == h2
     doAssert ds.s[ds[^1].v] == v2
-    echo ds.s[ds[^2].v]
     doAssert ds.s[ds[^2].n] == h
     doAssert ds.s[ds[^2].v] == v
 
@@ -309,7 +333,8 @@ when isMainModule:
     var
       ic = @[byte 0b00011111, 0b10011010]
       d = 0
-    doAssert(intdecode(ic, 5, d) == -1)
+    doAssertRaises(DecodeError):
+      discard intdecode(ic, 5, d)
   block:
     echo "Test Long lit int32"
     var
@@ -329,7 +354,8 @@ when isMainModule:
         0b11111111, 0b11111111,
         0b11111111, 0b01111111]
       d = 0
-    doAssert(intdecode(ic, 8, d) == -1)
+    doAssertRaises(DecodeError):
+      discard intdecode(ic, 8, d)
   block:
     echo "Test Bad lit int with too many zeros in the middle"
     var ic = @[byte 0b11111111]
@@ -337,7 +363,8 @@ when isMainModule:
       ic.add(0b10000000)
     ic.add(0b01111111)
     var d = 0
-    doAssert(intdecode(ic, 8, d) == -1)
+    doAssertRaises(DecodeError):
+      discard intdecode(ic, 8, d)
   block:
     echo "Test Lit int with too many starting zeros"
     var ic = @[byte 0b11111111]
@@ -463,7 +490,7 @@ when isMainModule:
         0x2d6b, 0x6579, 0x0d63, 0x7573,
         0x746f, 0x6d2d, 0x6865, 0x6164, 0x6572].toBytes
       d = initDecodedStr()
-      dh = initDeque[Header](32)
+      dh = initDynHeaders(255)
     doAssert(hdecode(ic, dh, d) == ic.len)
     var i = 0
     for h in d:
@@ -480,7 +507,7 @@ when isMainModule:
         0x040c'u16, 0x2f73, 0x616d,
         0x706c, 0x652f, 0x7061, 0x7468].toBytes
       d = initDecodedStr()
-      dh = initDeque[Header](32)
+      dh = initDynHeaders(255)
     doAssert(hdecode(ic, dh, d) == ic.len)
     var i = 0
     for h in d:
@@ -491,14 +518,13 @@ when isMainModule:
     doAssert dh.len == 0
   block:
     echo "Test Literal Header Field Never Indexed"
-    var
-      ic = @[
-        0x1008'u16, 0x7061, 0x7373, 0x776f,
-        0x7264, 0x0673, 0x6563, 0x7265].toBytes
+    var ic = @[
+      0x1008'u16, 0x7061, 0x7373, 0x776f,
+      0x7264, 0x0673, 0x6563, 0x7265].toBytes
     ic.add(byte 0x74'u8)
     var
       d = initDecodedStr()
-      dh = initDeque[Header](32)
+      dh = initDynHeaders(255)
     doAssert(hdecode(ic, dh, d) == ic.len)
     var i = 0
     for h in d:
@@ -512,7 +538,7 @@ when isMainModule:
     var
       ic = @[byte 0x82'u8]
       d = initDecodedStr()
-      dh = initDeque[Header](32)
+      dh = initDynHeaders(255)
     doAssert(hdecode(ic, dh, d) == ic.len)
     var i = 0
     for h in d:
@@ -521,31 +547,225 @@ when isMainModule:
       inc i
     doAssert i == 1
     doAssert dh.len == 0
+
+  proc hdecodeAll(
+      s: openArray[byte],
+      h: var DynHeaders,
+      d: var DecodedStr): int =
+    result = 0
+    while result < s.len:
+      inc(result, hdecode(toOpenArray(s, result, s.len-1), h, d))
+
   block:
-    echo "Test First Request"
-    var
-      ic = @[
-        0x8286'u16, 0x8441, 0x0f77, 0x7777,
-        0x2e65, 0x7861, 0x6d70, 0x6c65,
-        0x2e63, 0x6f6d].toBytes
-      d = initDecodedStr()
-      dh = initDeque[Header](32)
-      expected = [
-        [":method", "GET"],
-        [":scheme", "http"],
-        [":path", "/"],
-        [":authority", "www.example.com"]]
-      consumed = 0
-    while consumed < ic.len:
-      let c = hdecode(toOpenArray(ic, consumed, ic.len-1), dh, d)
-      doAssert c != -1
-      inc(consumed, c)
-    doAssert consumed == ic.len
-    var i = 0
-    for h in d:
-      doAssert(d.s[h.n] == expected[i][0])
-      doAssert(d.s[h.v] == expected[i][1])
-      inc i
-    doAssert i == expected.len
-    doAssert dh.len == 1
-    doAssert $dh == ":authority: www.example.com\r\L"
+    echo "Test Request Examples without Huffman Coding"
+    var dh = initDynHeaders(255)
+    block:
+      echo "Test First Request"
+      var
+        ic = @[
+          0x8286'u16, 0x8441, 0x0f77, 0x7777,
+          0x2e65, 0x7861, 0x6d70, 0x6c65,
+          0x2e63, 0x6f6d].toBytes
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "http"],
+          [":path", "/"],
+          [":authority", "www.example.com"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 1
+      doAssert $dh == ":authority: www.example.com\r\L"
+    block:
+      echo "Test Second Request"
+      var
+        ic = @[
+          0x8286'u16, 0x84be, 0x5808, 0x6e6f,
+          0x2d63, 0x6163, 0x6865].toBytes
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "http"],
+          [":path", "/"],
+          [":authority", "www.example.com"],
+          ["cache-control", "no-cache"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 2
+      doAssert($dh ==
+        "cache-control: no-cache\r\L" &
+        ":authority: www.example.com\r\L")
+    block:
+      echo "Test Third Request"
+      var ic = @[
+        0x8287'u16, 0x85bf, 0x400a, 0x6375,
+        0x7374, 0x6f6d, 0x2d6b, 0x6579,
+        0x0c63, 0x7573, 0x746f, 0x6d2d,
+        0x7661, 0x6c75].toBytes
+      ic.add(byte 0x65'u16)
+      var
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "https"],
+          [":path", "/index.html"],
+          [":authority", "www.example.com"],
+          ["custom-key", "custom-value"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 3
+      doAssert($dh ==
+        "custom-key: custom-value\r\L" &
+        "cache-control: no-cache\r\L" &
+        ":authority: www.example.com\r\L")
+
+  block:
+    echo "Test Request Examples with Huffman Coding"
+    var dh = initDynHeaders(256)
+    block:
+      echo "Test First Request"
+      var ic = @[
+        0x8286'u16, 0x8441, 0x8cf1, 0xe3c2,
+        0xe5f2, 0x3a6b, 0xa0ab, 0x90f4].toBytes
+      ic.add(byte 0xff'u16)
+      var
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "http"],
+          [":path", "/"],
+          [":authority", "www.example.com"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 1
+      doAssert $dh == ":authority: www.example.com\r\L"
+    block:
+      echo "Test Second Request"
+      var
+        ic = @[
+          0x8286'u16, 0x84be, 0x5886,
+          0xa8eb, 0x1064, 0x9cbf].toBytes
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "http"],
+          [":path", "/"],
+          [":authority", "www.example.com"],
+          ["cache-control", "no-cache"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 2
+      doAssert($dh ==
+        "cache-control: no-cache\r\L" &
+        ":authority: www.example.com\r\L")
+    block:
+      echo "Test Third Request"
+      var
+        ic = @[
+          0x8287'u16, 0x85bf, 0x4088, 0x25a8,
+          0x49e9, 0x5ba9, 0x7d7f, 0x8925,
+          0xa849, 0xe95b, 0xb8e8, 0xb4bf].toBytes
+        d = initDecodedStr()
+        expected = [
+          [":method", "GET"],
+          [":scheme", "https"],
+          [":path", "/index.html"],
+          [":authority", "www.example.com"],
+          ["custom-key", "custom-value"]]
+      doAssert hdecodeAll(ic, dh, d) == ic.len
+      var i = 0
+      for h in d:
+        doAssert(d.s[h.n] == expected[i][0])
+        doAssert(d.s[h.v] == expected[i][1])
+        inc i
+      doAssert i == expected.len
+      doAssert dh.len == 3
+      doAssert($dh ==
+        "custom-key: custom-value\r\L" &
+        "cache-control: no-cache\r\L" &
+        ":authority: www.example.com\r\L")
+
+    block:
+      echo "Test Response Examples without Huffman Coding"
+      var dh = initDynHeaders(255)
+      block:
+        echo "Test First Response"
+        var
+          ic = @[
+            0x4803'u16, 0x3330, 0x3258, 0x0770,
+            0x7269, 0x7661, 0x7465, 0x611d,
+            0x4d6f, 0x6e2c, 0x2032, 0x3120,
+            0x4f63, 0x7420, 0x3230, 0x3133,
+            0x2032, 0x303a, 0x3133, 0x3a32,
+            0x3120, 0x474d, 0x546e, 0x1768,
+            0x7474, 0x7073, 0x3a2f, 0x2f77,
+            0x7777, 0x2e65, 0x7861, 0x6d70,
+            0x6c65, 0x2e63, 0x6f6d].toBytes
+          d = initDecodedStr()
+          expected = [
+            [":status", "302"],
+            ["cache-control", "private"],
+            ["date", "Mon, 21 Oct 2013 20:13:21 GMT"],
+            ["location", "https://www.example.com"]]
+        doAssert hdecodeAll(ic, dh, d) == ic.len
+        var i = 0
+        for h in d:
+          doAssert(d.s[h.n] == expected[i][0])
+          doAssert(d.s[h.v] == expected[i][1])
+          inc i
+        doAssert i == expected.len
+        doAssert dh.len == 4
+        doAssert($dh ==
+          "location: https://www.example.com\r\L" &
+          "date: Mon, 21 Oct 2013 20:13:21 GMT\r\L" &
+          "cache-control: private\r\L" &
+          ":status: 302\r\L")
+      block:
+        echo "Test Second Response"
+        var
+          ic = @[
+            0x4803'u16, 0x3330, 0x37c1, 0xc0bf].toBytes
+          d = initDecodedStr()
+          expected = [
+            [":status", "307"],
+            ["cache-control", "private"],
+            ["date", "Mon, 21 Oct 2013 20:13:21 GMT"],
+            ["location", "https://www.example.com"]]
+        doAssert hdecodeAll(ic, dh, d) == ic.len
+        var i = 0
+        for h in d:
+          doAssert(d.s[h.n] == expected[i][0])
+          doAssert(d.s[h.v] == expected[i][1])
+          inc i
+        doAssert i == expected.len
+        doAssert dh.len == 4
+        doAssert($dh ==
+          ":status: 307\r\L" &
+          "location: https://www.example.com\r\L" &
+          "date: Mon, 21 Oct 2013 20:13:21 GMT\r\L" &
+          "cache-control: private\r\L")
